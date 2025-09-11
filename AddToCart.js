3cd6b8a921682fe316da13bd34ca86f55e0ceeb8
Suresh-Bala-connect <msugu76@gmail.com>
2025-09-11 18:17:05 +0530

    Add files via upload

--- AddToCart.js
+++ AddToCart.js
@@ -2,73 +2,92 @@ import React, { useEffect, useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import { FaMinus } from "react-icons/fa";
 import { LuIndianRupee } from "react-icons/lu";
-// import Nav_bar from "./Nav_bar";
+import Nav_bar from "./Nav_bar";
 import './AddToCart.css';
 
 function AddToCart() {
 
     const [tableCart, setTableCart] = useState(JSON.parse(localStorage.getItem("addToList")) || []);
-    // console.log(tableCart);
-
-    const [getCount,setCount]=useState(1)
-    // var tableCart;
+   
     var inc_number;
-    function handleIncrr(name){
-        setCount(getCount+1)
-        // console.log(e.target.value)
-
-         inc_number=tableCart.find((pro_name)=>{
-            return(
-                pro_name.name==name
-            )
-            // if(pro_name.name==name){
-            //     setCount(getCount+1)
-            // }
+    function handleIncrr(product) {
+        inc_number = tableCart.map((data) => {
+            if (product.id == data.id) {
+                console.log(data.name)
+                if (data.qty > 0) {
+                    return { ...data, qty: data.qty + 1 }
+                }
+                console.log(data.name)
+               
+            }
+            return (data)
         })
-        console.log(inc_number)
-        inc_number.qty=Number(getCount)
-        localStorage.setItem("addToList",JSON.stringify(tableCart));
-        console.log(getCount)
-         setTableCart(tableCart)
-         console.log(tableCart)
-    }
-     function handeleDecrr(name){
-        setCount(getCount-1)
-         var dec_number=tableCart.find((pro_name)=>{
-            return(
-                pro_name.name==name
-            )
-        })
-        console.log(dec_number)
+        setTableCart(inc_number)
+        console.log(tableCart)
     }
 
+ var dec_number;
+    function handeleDecrr(product) {
+         dec_number = tableCart.map((data) => {
+            if (product.id == data.id) {
+                console.log(data.name)
+                if (data.qty > 1) {
+                    return { ...data, qty: data.qty -1 }
+                }
+                console.log(data.name) 
+            }
+            return (data)
+        })
+        setTableCart(dec_number)
+        console.log(tableCart)
+        
+    }
+
+    useEffect(() => {
+        var changedCartData = tableCart.map((item) => {
+            return { ...item, qty: 1 }
+        })
+
+        setTableCart(changedCartData);
+    }, [])
+
+    const subTotal = tableCart.reduce((acc, item) => {
+    return acc + (item.price * item.qty);
+        }, 0);
+
+   
     return (
         <div>
-             {/* <Nav_bar/> */}
+            {/* <Nav_bar/> */}
             <div className="add_list">
-                
+                 <div className="nav_bar">
+                    <Nav_bar/>
+                </div>
                 {
                     tableCart.map((item) => {
                         return (
-                            
                             <div className="">
-                                <div className="list">
-                                     <img src={item.img}></img>
-                                    {/* <img src={item.img}  alt="img" width={200}/> */}
+                                <div className="list" key={item.id}>
+                                    <img src={item.img}></img>
                                     <p>{item.name}</p>
                                     <p><LuIndianRupee />{item.price} /per kg</p>
+                                    <p>Total Price : <LuIndianRupee />{item.price*item.qty}</p>
                                 </div>
                                 <div className="qty">
-                                    <h4>Qty</h4>
-                                    <button onClick={()=>handleIncrr(item.name)}><FaPlus /></button>
-                                    <p>{getCount}</p>
-                                    <button onClick={()=>handeleDecrr(item.name)}><FaMinus /></button>
+                                    <button onClick={() => handleIncrr(item)}><FaPlus /></button>
+                                    <p>{item.qty}</p>
+                                    <button onClick={() => handeleDecrr(item)}><FaMinus /></button>
                                 </div>
-                                
-                            </div> 
+                               
+
+
+                            </div>
                         )
                     })
                 }
+            </div>
+             <div className="cart-summary">
+                <h3>Sub Total: <LuIndianRupee />{subTotal}</h3>
             </div>
         </div>
     )
